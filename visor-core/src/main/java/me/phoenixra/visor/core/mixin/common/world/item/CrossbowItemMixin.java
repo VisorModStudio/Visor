50570d9601236da2b7aaf02713db0726ae59664b
fenix <fenixra1332@gmail.com>
2025-07-15 00:32:02 +0400

    renamed mixin method to stat with 'visor$'

--- visor-core/src/main/java/me/phoenixra/visor/core/mixin/common/world/item/CrossbowItemMixin.java
+++ visor-core/src/main/java/me/phoenixra/visor/core/mixin/common/world/item/CrossbowItemMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public class CrossbowItemMixin {
 
     @WrapOperation(method = "shootProjectile", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getViewVector(F)Lnet/minecraft/world/phys/Vec3;"))
-    private static Vec3 vivecraft$vrAim(LivingEntity instance, float partialTicks, Operation<Vec3> original) {
+    private static Vec3 visor$vrAim(LivingEntity instance, float partialTicks, Operation<Vec3> original) {
         if (instance instanceof ServerPlayer player) {
             VRServerPlayer serverPlayer = VisorAPI.server().getVrPlayer(player);
             if (serverPlayer == null || !serverPlayer.isVr()) {
