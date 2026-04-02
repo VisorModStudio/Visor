01a4458a41b7dcfb309cfab0beedea5772492b36
Weever1337 <weever1337@gmail.com>
2026-04-02 16:09:14 +0300

    a bit improved crosshair in VR

--- visor-core/src/main/java/org/vmstudio/visor/core/client/render/decoration/effects/hand/HandEffectCrosshair.java
+++ visor-core/src/main/java/org/vmstudio/visor/core/client/render/decoration/effects/hand/HandEffectCrosshair.java
@@ -34,7 +34,6 @@ import org.lwjgl.opengl.GL11C;
 
 import static org.vmstudio.visor.core.client.VisorClientImpl.MC;
 
-//@TODO fix: on forge for some reason one eye define that crosshair is bright, the other don't
 @RegisterVRHandEffect
 public class HandEffectCrosshair extends VRHandEffect {
     public static final String ID = "crosshair";
@@ -67,9 +66,6 @@ public class HandEffectCrosshair extends VRHandEffect {
         // nudge back for correct lighting
         var crossPos = rawCross.add(aim.normalize().mul(LIGHT_OFFSET));
 
-        // light & brightness
-        BlockPos lightPos = BlockPos.containing(new Vec3(crossPos));
-        int lightCoords  = LevelRenderer.getLightColor(MC.level, lightPos);
         float brightness = (MC.hitResult == null || MC.hitResult.getType() == HitResult.Type.MISS)
                 ? MISS_BRIGHTNESS
                 : FULL_BRIGHTNESS;
@@ -77,22 +73,22 @@ public class HandEffectCrosshair extends VRHandEffect {
         BufferBuilder buf = Tesselator.getInstance().getBuilder();
 
         // --- GL setup ---
-        RenderSystem.setShaderColor(1, 1, 1, 1);
-        MC.gameRenderer.lightTexture().turnOnLightLayer();
-        RenderSystem.depthMask(true);
-        RenderSystem.enableDepthTest();
-        RenderSystem.depthFunc(GL11C.GL_LEQUAL);
+        RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+
+        RenderSystem.disableDepthTest();
+        RenderSystem.depthMask(false);
+        RenderSystem.depthFunc(GL11C.GL_ALWAYS);
 
         RenderSystem.enableBlend();
         RenderSystem.blendFuncSeparate(
                 GlStateManager.SourceFactor.ONE_MINUS_DST_COLOR,
-                GlStateManager.DestFactor.ZERO,
+                GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR,
                 GlStateManager.SourceFactor.ONE,
-                GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA
+                GlStateManager.DestFactor.ZERO
         );
 
         RenderSystem.setShaderTexture(0, ICONS_LOC);
-        RenderSystem.setShader(GameRenderer::getRendertypeEntityCutoutNoCullShader);
+        RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
 
         // --- Pose setup ---
         poseStack.pushPose();
@@ -108,35 +104,33 @@ public class HandEffectCrosshair extends VRHandEffect {
         poseStack.scale(scale, scale, scale);
 
         // --- Render ---
-        buf.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.NEW_ENTITY);
+        buf.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX_COLOR);
         Matrix4f mat = poseStack.last().pose();
 
-        buf.vertex(mat, -1f,  1f, 0f)
-                .color(brightness, brightness, brightness, 1f)
+        buf.vertex(mat, -1f,1f,0f)
                 .uv(UV_SIZE, 0f)
-                .overlayCoords(OverlayTexture.NO_OVERLAY).uv2(lightCoords)
-                .normal(0f, 0f, 1f).endVertex();
-        buf.vertex(mat,  1f,  1f, 0f)
                 .color(brightness, brightness, brightness, 1f)
-                .uv(0f,       0f)
-                .overlayCoords(OverlayTexture.NO_OVERLAY).uv2(lightCoords)
-                .normal(0f, 0f, 1f).endVertex();
-        buf.vertex(mat,  1f, -1f, 0f)
+                .endVertex();
+        buf.vertex(mat,1f,1f,0f)
+                .uv(0f,0f)
                 .color(brightness, brightness, brightness, 1f)
-                .uv(0f,       UV_SIZE)
-                .overlayCoords(OverlayTexture.NO_OVERLAY).uv2(lightCoords)
-                .normal(0f, 0f, 1f).endVertex();
-        buf.vertex(mat, -1f, -1f, 0f)
+                .endVertex();
+        buf.vertex(mat,1f, -1f, 0f)
+                .uv(0f, UV_SIZE)
                 .color(brightness, brightness, brightness, 1f)
+                .endVertex();
+        buf.vertex(mat,-1f, -1f,0f)
                 .uv(UV_SIZE, UV_SIZE)
-                .overlayCoords(OverlayTexture.NO_OVERLAY).uv2(lightCoords)
-                .normal(0f, 0f, 1f).endVertex();
+                .color(brightness, brightness, brightness, 1f)
+                .endVertex();
 
         BufferUploader.drawWithShader(buf.end());
 
         // --- Restore GL & pose ---
         RenderSystem.defaultBlendFunc();
         RenderSystem.disableBlend();
+        RenderSystem.depthMask(true);
+        RenderSystem.enableDepthTest();
         RenderSystem.depthFunc(GL11C.GL_LEQUAL);
         poseStack.popPose();
     }
@@ -144,7 +138,7 @@ public class HandEffectCrosshair extends VRHandEffect {
     private void applyCrossHairRotation(PoseStack poseStack,
                                         HandType hand,
                                         VRPlayerPoseClient pose) {
-        if (MC.hitResult instanceof BlockHitResult bhr) {
+        if (MC.hitResult instanceof BlockHitResult bhr && bhr.getType() != HitResult.Type.MISS) {
             switch (bhr.getDirection()) {
                 case DOWN -> {
                     rotateInDegrees(poseStack, pose.getHand(hand).getYawDegrees(), 0, 1, 0);
