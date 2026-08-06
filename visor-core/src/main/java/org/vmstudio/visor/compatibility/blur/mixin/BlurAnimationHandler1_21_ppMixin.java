e4c686e9c7c8cebc81cafb997dc6ff5f7f5f5bac
Weever <109693401+Weever1337@users.noreply.github.com>
2026-08-07 01:13:37 +0300

    feat: blur compatibility for 1.21+ and 1.20.x

---
+++ visor-core/src/main/java/org/vmstudio/visor/compatibility/blur/mixin/BlurAnimationHandler1_21_ppMixin.java
@@ -0,0 +1,23 @@
+package org.vmstudio.visor.compatibility.blur.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.vmstudio.visor.compatibility.ClassDependentMixin;
+import org.vmstudio.visor.core.client.VisorState;
+
+// Works for 1.21.x -> 26.x (midnightdust version, Blur+)
+@Pseudo
+@ClassDependentMixin("eu.midnightdust.blur.animations.AbstractAnimationHandler")
+@Mixin(targets = "eu.midnightdust.blur.animations.AbstractAnimationHandler", remap = false)
+public class BlurAnimationHandler1_21_ppMixin {
+    @Inject(method = "getCurrentValue", at = @At("HEAD"), cancellable = true, remap = false)
+    private void visor$dropBlurRadius(CallbackInfoReturnable<Float> cir) {
+        if (VisorState.get().isNotActive()) return;
+        if (!getClass().getName().endsWith("BlurRadiusAnimationHandler")) return;
+
+        cir.setReturnValue(0.0F);
+    }
+}
