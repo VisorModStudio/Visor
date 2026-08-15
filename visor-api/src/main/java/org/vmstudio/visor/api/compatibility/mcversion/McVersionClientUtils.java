142a0251f2435b0a1634fd5e54ce0562c7842097
Bogdan <53151806+Phoenix-Ra@users.noreply.github.com>
2026-08-15 16:15:33 +0400

    feat(mcversion): added McVersionClientUtils

---
+++ visor-api/src/main/java/org/vmstudio/visor/api/compatibility/mcversion/McVersionClientUtils.java
@@ -0,0 +1,19 @@
+package org.vmstudio.visor.api.compatibility.mcversion;
+
+import net.minecraft.client.gui.screens.GenericDirtMessageScreen;
+import net.minecraft.client.gui.screens.ProgressScreen;
+import net.minecraft.client.gui.screens.ReceivingLevelScreen;
+import net.minecraft.client.gui.screens.Screen;
+import org.jetbrains.annotations.Nullable;
+
+public class McVersionClientUtils {
+    private McVersionClientUtils() {
+        throw new UnsupportedOperationException("This is an utility class and cannot be instantiated");
+    }
+
+    public static boolean isLevelTransitionScreen(@Nullable Screen screen) {
+        return screen instanceof ReceivingLevelScreen
+                || screen instanceof ProgressScreen
+                || screen instanceof GenericDirtMessageScreen;
+    }
+}
