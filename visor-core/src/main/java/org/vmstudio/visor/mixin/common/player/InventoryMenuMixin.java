5740ad51268cc1ac27214afbaf41db7e99e40f8c
Bogdan <53151806+Phoenix-Ra@users.noreply.github.com>
2026-08-17 18:53:07 +0400

    fix: vanilla offhand slot is active when twoHandedVR enabled

--- visor-core/src/main/java/org/vmstudio/visor/mixin/common/player/InventoryMenuMixin.java
+++ visor-core/src/main/java/org/vmstudio/visor/mixin/common/player/InventoryMenuMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.world.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.vmstudio.visor.core.common.player.OffhandSlot;
 
 @Mixin(InventoryMenu.class)
@@ -16,13 +15,17 @@ public class InventoryMenuMixin {
             method = "<init>",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/inventory/InventoryMenu;addSlot(Lnet/minecraft/world/inventory/Slot;)Lnet/minecraft/world/inventory/Slot;",
-                    ordinal = 5
-            )
+                    target = "Lnet/minecraft/world/inventory/InventoryMenu;addSlot(Lnet/minecraft/world/inventory/Slot;)Lnet/minecraft/world/inventory/Slot;"
+            ),
+            require = 1
     )
     private Slot visor$replaceOffhandSlot(Slot original) {
+        if (!(original.container instanceof Inventory inventory)
+                || original.getContainerSlot() != Inventory.SLOT_OFFHAND) {
+            return original;
+        }
         return new OffhandSlot(
-                ((Inventory) original.container).player,
+                inventory.player,
                 original.container,
                 original.getContainerSlot(),
                 original.x,
